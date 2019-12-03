27c2cebe808b822677237ccec57c26970cbac2c3
John Weber <johnhenryweber@gmail.com>
2019-12-03 15:31:19 -0800

    Webscraping Complete

---
+++ webscraping/webscraping.js
@@ -0,0 +1,29 @@
+const cheerio = require('cheerio');
+const axios = require('axios');
+const fs = require('fs');
+
+axios.get('https://presidenstory.com/stat_pht.php').then((response) => {
+  const $ = cheerio.load(response.data);
+
+  const urlElems = $('table.yng tbody tr td img');
+
+  let presidentalArray = [];
+
+  for (let i = 0; i < urlElems.length; i++) {
+    if (!(urlElems[i].attribs.alt.includes("young"))) {
+      let newPresident = {
+        name: urlElems[i].attribs.alt,
+        image: "http://www.presidenstory.com/" + urlElems[i].attribs.src,
+        checked: false
+      }
+
+      presidentalArray.push(newPresident);
+    }
+  }
+
+  fs.writeFile('allPresidents.txt', JSON.stringify(presidentalArray, null, 2), (err) => {
+    if (err) throw err;
+
+    console.log("Saved");
+  });
+});
