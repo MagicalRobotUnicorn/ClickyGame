759adbf4d24d24023e624e57d05ec21628c9cf9e
John Weber <johnhenryweber@gmail.com>
2019-12-07 19:25:44 -0800

    Arrays of Presidents Work

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -4,11 +4,31 @@ import President from './President';
 export class gameBoard extends Component {
 
   render() {
+    let arrayOne = [];
+    let arrayTwo = [];
+    let arrayThree = [];
+    let arrayFour = [];
+    let arrayFive = [];
+    
+    let allArrays = [arrayOne, arrayTwo, arrayThree, arrayFour, arrayFive];
+
+    let i = 0;
+    for (let j = 0; j < allArrays.length; j++){
+      for (let k= 0; k < 9; k++){
+        (allArrays[j]).push(this.props.Presidents[i]);
+        i++;
+      }
+    }
+
+    console.log(allArrays);
+    
     return (
-      this.props.Presidents.map((president) => (
+      <div class="row">
+      {this.props.Presidents.map((president) => (
         <President key={president.id} president={president} />
-      )));
-  }
+      ))}
+      </div>
+    )}
 }
 
 export default gameBoard
