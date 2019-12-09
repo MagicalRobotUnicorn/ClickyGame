4c27ba961f7aa28533aea8823047f02e3d7daeb4
John Weber <johnhenryweber@gmail.com>
2019-12-09 13:49:43 -0800

    Medium Publishing Instructions

---
+++ docs/build/precache-manifest.5098121dd5d552584fbb99940cc38c29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "010472ea888cfc750d48f7f66debd3ec",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "ede60e3d6d67be26eb98",
+    "url": "/ClickyGame/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ede60e3d6d67be26eb98",
+    "url": "/ClickyGame/static/js/2.72299af1.chunk.js"
+  },
+  {
+    "revision": "0b3f12142bd70cb1250d",
+    "url": "/ClickyGame/static/js/main.9f161143.chunk.js"
+  },
+  {
+    "revision": "657d5971a35598ccfe89",
+    "url": "/ClickyGame/static/js/runtime-main.98ccdd34.js"
+  }
+]);
